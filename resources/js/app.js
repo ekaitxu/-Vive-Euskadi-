9177d172618a829391b5bca6615cbe2c705ed8d9
María <magomez294@gmail.com>
2022-01-10 11:45:31 +0100

    Creación vista admin

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,15 @@ require('./bootstrap');
 import Vue from 'vue'
 
 // importar componente
-import SobreNosotros from './components/SobreNosotros.vue'
+import SobreNosotros from './components/SobreNosotros.vue';
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 // crear instancia vue
 Vue.component('sobre-nosotros', require('./components/SobreNosotros.vue').default);
+Vue.component('comp-admin', require('./components/adminComp.vue').default);
+
+// creating a vue instance
+const app = new Vue({
+    el: '#app'
+});
